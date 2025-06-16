d4d910bd4c773072ef9cb08daacb493323b85f22
willGyu <jgt8731@itwillbs.co.kr>
2025-06-16 09:55:25 +0900

    컨트롤러에 listALL 기능 구현 & view페이지 연결

--- SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
@@ -36,7 +36,7 @@ public class BoardController {
 	
 	// 글쓰기 (정보 입력) / GET
 	@RequestMapping(value = "/regist",method = RequestMethod.GET)
-	public void boardRegistGET() {
+	public void boardRegistGET() throws Exception{
 		logger.info(" boardRegistGET() 실행 ");
 		logger.info(" /views/board/regist.jsp 페이지 이동 ");
 	}
@@ -60,6 +60,16 @@ public class BoardController {
 		return "redirect:/board/listALL";
 	}
 	
+	
+	// http://localhost:8088/board/listALL
+	// 게시판 리스트 (ALL)
+	@RequestMapping(value = "/listALL",method = RequestMethod.GET)
+	public void boardListAllGET() throws Exception {
+		logger.info(" boardListAllGET() 실행");
+		
+		
+		logger.info(" /views/board/listALL.jsp 페이지 연결");
+	}
 	
 	
 	
